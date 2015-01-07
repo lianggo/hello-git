a7c6990ef75b919d5fdb9970c8bc86663a5cc863
lianggo <lianggo@gmail.com>
2015-01-07 13:16:20 +0800

    使用Eclipse提交

--- hello-git/src/net/lianggo/HelloGit.java
+++ hello-git/src/net/lianggo/HelloGit.java
@@ -3,7 +3,7 @@ package net.lianggo;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("hello, git!");
+		System.out.println("hello, git!");//commit with eclipse
 	}
 
 }
